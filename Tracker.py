d5ee90fbe169396d917ca2bdc12613905dcd285b
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-01-21 20:11:28 -0800

    pretty writing and better data arrangement

--- Tracker.py
+++ Tracker.py
@@ -2,14 +2,28 @@ import datetime, json
 class Tracker:
     def __init__(self):
         self.moodFile = json.loads(open('moodData.json').read())
+        
     def add(self):
-        key = input('what happened:   ' )
-        key = str(datetime.datetime.now()) + ' ' + key
+        event = input('what happened:   ' )
+        date = str(datetime.datetime.now())
         value = int(input('give it a score: '))
-        self.moodFile.update({key: value})
+        self.moodFile.update(
+            {
+                date:{
+                    event: value
+                }
+            }
+        )
+        
     def save(self):
-        with open('moodData.json') as saveFile:
-            saveFile.write(str(json.dumps(self.moodFile)))
+        with open('moodData.json', 'w') as saveFile:
+            saveFile.write(
+                json.dumps(
+                    self.moodFile,
+                    indent = 2,
+                    sort_keys = True
+                )
+            )
             
 if __name__ == '__main__':
     myTracker = Tracker()
